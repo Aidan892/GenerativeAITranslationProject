b4ee56e9c2a3a4b39794777b651330b0ac42b12a
Aidan Tam <144278081+atam283@users.noreply.github.com>
2024-12-23 16:01:19 -0800

    Scripts completed

--- translation.py
+++ translation.py
@@ -1,14 +1,22 @@
-print("This is a test")
+from openai import OpenAI
+import sys
+import json
 
-# from openai import OpenAI
+if __name__ == "__main__":
+    filePath = sys.argv[1]
 
-# client = OpenAI()
+    with open(filePath, 'r') as configFile:
+        configData = json.load(configFile)
+        OPEN_AI_KEY = configData["OPEN_AI_KEY"]
+        configFile.close()
 
-# stream = client.chat.completions.create(
-#     model="gpt-4o-mini",
-#     messages=[{"role": "user", "content": "Say this is a test"}],
-#     stream=True,
-# )
-# for chunk in stream:
-#     if chunk.choices[0].delta.content is not None:
-#         print(chunk.choices[0].delta.content, end="")
+    client = OpenAI(api_key = OPEN_AI_KEY)
+
+    stream = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": "Say this is a test"}],
+        stream=True,
+    )
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end="")
