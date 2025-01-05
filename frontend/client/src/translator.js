583c0c5f86faaf077eaa16bf60ee153e15b5cb91
Aidan Tam <144278081+atam283@users.noreply.github.com>
2025-01-04 20:36:20 -0800

    Website style changes

--- frontend/client/src/translator.js
+++ frontend/client/src/translator.js
@@ -1,12 +1,11 @@
 import './style.css';
 import './main.js';
 
-
 export function loadTranslator() {
-    document.querySelector('#app').innerHTML = `
-      <div>
-        <textarea class="chat-box1" placeholder="Insert code here..."></textarea>
-        <textarea class="chat-box2"></textarea>
-      </div>
-    `;
-  }
+  document.querySelector('#app').innerHTML = `
+    <div>
+      <textarea class="chat-box1" placeholder="Insert code here..."></textarea>
+      <textarea class="chat-box2"></textarea>
+    </div>
+  `;
+}
